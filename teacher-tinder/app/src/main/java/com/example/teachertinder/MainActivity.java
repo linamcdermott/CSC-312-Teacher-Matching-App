7d388c20a31327344723f5ba5a170c517c5f2cc0
Lina McDermott <limcdermott@davidson.edu>
2020-04-16 15:35:20 -0700

    Add DashboardActivity
    Adds a button to navigate to the DashboardActivity from MainActivity, as well as classes for the DashboardActivity, DashboardFragment, and DashboardViewModel.

--- teacher-tinder/app/src/main/java/com/example/teachertinder/MainActivity.java
+++ teacher-tinder/app/src/main/java/com/example/teachertinder/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.teachertinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.dashboard_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Intent intent = new Intent(context, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
